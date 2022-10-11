4bc2271e98f5ef88ef61732177bda5d04f74734b
lukyanenko01 <v.lukyanenko0101@gmail.com>
2022-10-11 17:36:18 +0300

    authorization screen

--- PizzaDelivery/ContentView.swift
+++ PizzaDelivery/ContentView.swift
@@ -8,9 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var nomber = ""
+    @State private var password = ""
+
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Авторизація")
+                .padding()
+                .padding(.horizontal, 30)
+                .font(.title2.bold())
+                .background(Color("whiteAlpha"))
+                .cornerRadius(30)
+            
+            VStack {
+                TextField("Номер телефону", text: $nomber)
+                    .padding()
+                    .background(Color("whiteAlpha"))
+                    .cornerRadius(12)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+                
+                SecureField("Введіть пароль", text: $password)
+                    .padding()
+                    .background(Color("whiteAlpha"))
+                    .cornerRadius(12)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+
+               
+                    
+                    Button {
+                        print("Увійти")
+                    } label: {
+                        Text("Увійти")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(LinearGradient(colors: [.yellow, .orange], startPoint: .leading, endPoint: .trailing))
+                            .cornerRadius(12)
+                            .padding(8)
+                            .padding(.horizontal, 12)
+                            .font(.title3.bold())
+                            .foregroundColor(Color("brown"))
+                    }
+
+                Button {
+                     print("Реєстрація")
+                } label: {
+                    Text("Реєстрація")
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity)
+                        //.background(LinearGradient(colors: [.yellow, .orange], startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(12)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(Color("brown"))
+                }
+
+                
+            }
             .padding()
+            .padding(.top, 16)
+            .background(Color("whiteAlpha"))
+            .cornerRadius(12)
+            .padding()
+            //.padding(30)
+            
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Image("pizza1").ignoresSafeArea())
     }
 }
 
