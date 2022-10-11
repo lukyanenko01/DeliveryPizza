822ed6a5b0a6db15182ed7226d019b7d1d1992c4
lukyanenko01 <v.lukyanenko0101@gmail.com>
2022-10-11 15:53:39 +0300

    Initial Commit

---
+++ PizzaDelivery/PizzaDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaDeliveryApp.swift
+//  PizzaDelivery
+//
+//  Created by Lukich on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PizzaDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
