993d6b0ece15650c7582447e6f889a79a9b72c86
lukyanenko01 <v.lukyanenko0101@gmail.com>
2022-10-13 11:02:45 +0300

    profile screen

--- PizzaDelivery/View/PersonView.swift
+++ PizzaDelivery/View/PersonView.swift
@@ -8,8 +8,79 @@
 import SwiftUI
 
 struct PersonView: View {
+    
+    @State var isAvaAlertPresented = false
+    @State var isQuitAlertPresented = false
+    @State var isAuthViewPresented = false
+    
     var body: some View {
-        Text("Person")
+        VStack(spacing: 20) {
+            HStack(spacing: 16) {
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                    .padding(8)
+                    .background(Color("grey"))
+                    .clipShape(Circle())
+                    .onTapGesture {
+                        isAvaAlertPresented.toggle()
+                    }
+                    .confirmationDialog("Взяти фото з", isPresented: $isAvaAlertPresented) {
+                        Button {
+                            print("Library")
+                        } label: {
+                            Text("З галереї")
+                        }
+                        
+                        Button {
+                            print("Камера")
+                        } label: {
+                            Text("З камери")
+                        }
+                        
+
+                    }
+                
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("Чувак Любительпіци")
+                        .bold()
+                    Text("098 999 99 99")
+                }
+                Spacer()
+            }.padding(.horizontal)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Адес доставки: ")
+                    .bold()
+                Text("м. Зароріжья, вул. Шевченка 123")
+            }//.padding(.horizontal)
+            
+            //MARK: - таблица с заказами
+            List {
+                Text("Твої замовлення будуть тут")
+            } .listStyle(.plain)
+            
+            Button {
+                isQuitAlertPresented.toggle()
+            } label: {
+                Text("Вийти")
+                    .padding()
+                    .padding(.horizontal, 30)
+                    .background(Color.red)
+                    .cornerRadius(20)
+                    .foregroundColor(.white)
+            }.padding()
+                .confirmationDialog("Вийти?", isPresented: $isQuitAlertPresented) {
+                    Button {
+                        isAuthViewPresented.toggle()
+                    } label: {
+                        Text("Так")
+                    }
+                }
+                .fullScreenCover(isPresented: $isAuthViewPresented, onDismiss: nil) {
+                    AuthView()
+                }
+        }
     }
 }
 
